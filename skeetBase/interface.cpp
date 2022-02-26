8ba8969334025ed4e0fad869756ed6f9fb7127bf
Ethan Jamison <range.picker@gmail.com>
2022-02-26 10:47:51 -0700

    Update interface.cpp
    Beginning to interface changes

--- skeetBase/interface.cpp
+++ skeetBase/interface.cpp
@@ -8,13 +8,18 @@
 #include "Interface.h"
 
 void Interface::input(const UserInput &ui) {
-   
+   lgoic.input(isUp() + ui.isRight(), ui.isDown() + ui.isLeft(), ui.isSpace(), ui.isM(), ui.isB());
 }
 
 void Interface::processing() {
-   
+   if (logic.isPlaying())
+      logic.spawn();
+   logic.advance();
 }
 
 void Interface::output() {
-   
+   if (!logic.isPlaying())
+   {
+      bool isGameOver = logic.isGameOver();
+      double percentLEft = logic.getPercentLeft();
 }
